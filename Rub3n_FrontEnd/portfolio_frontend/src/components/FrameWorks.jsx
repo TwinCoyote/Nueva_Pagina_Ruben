622bad262a7f9c1e0e7903a531b81957ae57c9ba
TwinCoyote <twincoyote0732@gmail.com>
2025-10-21 10:06:46 -0600

    Adding logos and fix FrameWorks.jsx

--- Rub3n_FrontEnd/portfolio_frontend/src/components/FrameWorks.jsx
+++ Rub3n_FrontEnd/portfolio_frontend/src/components/FrameWorks.jsx
@@ -2,31 +2,31 @@ import { OrbitingCircles } from "./OrbitingCircles";
 
 export function Frameworks() {
   const skills = [
-    "auth0",
-    "blazor",
     "cplusplus",
-    "csharp",
     "css3",
-    "dotnet",
-    "dotnetcore",
+    "sie",
+    "linux",
     "git",
     "html5",
     "javascript",
+    "Auto",
     "microsoft",
     "react",
     "sqlite",
     "tailwindcss",
     "vitejs",
-    "wordpress",
+    "py",
+    "Altium",
+    "Django",
   ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
-      <OrbitingCircles iconSize={40}>
+      <OrbitingCircles iconSize={45}>
         {skills.map((skill, index) => (
           <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
-      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+      <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
         {skills.reverse().map((skill, index) => (
           <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
--- Rub3n_FrontEnd/portfolio_frontend/src/sections/About.jsx
+++ Rub3n_FrontEnd/portfolio_frontend/src/sections/About.jsx
@@ -17,10 +17,10 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Ali Sanati</p>
+            <p className="headtext">Hi, I'm Ruben R.</p>
             <p className="subtext">
-              Over the last 4 years, I developed my frontend and backend dev
-              skills to deliver dynamic and software and web applications.
+              Over the last year, I developed my frontend and backend dev
+              skills to deliver dynamic web applications and embedded projects.
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
@@ -32,46 +32,51 @@ const About = () => {
             className="flex items-center justify-center w-full h-full"
           >
             <p className="flex items-end text-5xl text-gray-500">
-              CODE IS CRAFT
+              Microcontrollers Stack
             </p>
             <Card
               style={{ rotate: "75deg", top: "30%", left: "20%" }}
-              text="GRASP"
+              text="Analog Electronic"
               containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
-              text="SOLID"
+              style={{ rotate: "30deg", top: "40%", left: "90%" }}
+              image="assets/logos/micro.png"
               containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
-              text="Design Patterns"
+              style={{ rotate: "50deg", top: "10%", left: "50%" }}
+              text="Digital Systems"
               containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
-              text="Design Principles"
+              style={{ rotate: "30deg", bottom: "10%", left: "70%" }}
+              text="PCB Design"
               containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "20deg", top: "10%", left: "38%" }}
-              text="SRP"
+              style={{ rotate: "30deg", top: "40%", left: "10%" }}
+              image="assets/logos/arduino.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "20%", left: "30%" }}
+              image="assets/logos/siu.png"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "30deg", top: "70%", left: "70%" }}
-              image="assets/logos/csharp-pink.png"
+              image="assets/logos/Cmas.png"
               containerRef={grid2Container}
             />
             <Card
-              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
-              image="assets/logos/dotnet-pink.png"
+              style={{ rotate: "0deg", top: "70%", left: "25%" }}
+              image="assets/logos/ESP32.svg"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-45deg", top: "5%", left: "10%" }}
-              image="assets/logos/blazor-pink.png"
+              image="assets/logos/raspberry-pi.svg"
               containerRef={grid2Container}
             />
           </div>
@@ -106,8 +111,8 @@ const About = () => {
           <div className="z-10 w-[50%]">
             <p className="headText">Teck Stack</p>
             <p className="subtext">
-              I specialize in a variety of languages, frameworks, and tools taht
-              allow me to build robust and scalable applications
+              I specialize in embedded systems, electronics, and software development — combining
+              microcontroller programming, circuit design, and modern frameworks to create efficient and scalable solutions.
             </p>
           </div>
           <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
