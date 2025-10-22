bed58c0348ec8fc97e5543d86b93d3f4c8db1dcb
TwinCoyote <twincoyote0732@gmail.com>
2025-10-22 11:36:47 -0600

    Refactor HeroText component: update text content to English and enhance word list

--- Rub3n_FrontEnd/portfolio_frontend/src/components/HeroText.jsx
+++ Rub3n_FrontEnd/portfolio_frontend/src/components/HeroText.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { FlipWords } from "./FlipWords";
 import { motion } from "motion/react";
 
-
 export const HeroText = () => {
-    const words = ["Seguro", "Moderno", "Escalable"];
-    const variants = {
-        hidden: {opacity: 0, x: -50},
-        visible: {opacity: 1, x: 0},
-    }
+  const words = [
+    "Secure",
+    "Robust",
+    "Reliable",
+    "Adaptative",
+    "Precise",
+    "Innovative",
+    "Dynamic",
+    "Custom",
+  ];
+  const variants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 },
+  };
   return (
-    
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       {/* Vista de Escitorio */}
       <div className=" flex-col hidden md:flex c-space">
@@ -21,15 +28,17 @@ export const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hola! Soy Ruben
+          Hi! I'm Ruben
         </motion.h1>
         <div className="flex flex-col items-start">
-          <motion.p className="text-5xl font-medium text-neutral-300"
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          transition={{ delay: 1.2 }}>
-            Un Desarrollador <br /> Dedicado a Crear
+          <motion.p
+            className="text-5xl font-medium text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.2 }}
+          >
+            A Developer <br /> Dedicated to Creating
           </motion.p>
           <div>
             <FlipWords
@@ -37,11 +46,13 @@ export const HeroText = () => {
               className="font-black text-white text-8xl"
             />
           </div>
-          <motion.p className="text-4xl font-medium text-neutral-300"
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          transition={{ delay: 1.4 }}>
+          <motion.p
+            className="text-4xl font-medium text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.4 }}
+          >
             Embedded Solutions
           </motion.p>
         </div>
@@ -49,17 +60,24 @@ export const HeroText = () => {
 
       {/* Vista de Celular */}
       <div className="flex- flex-col space-y-6 md:hidden">
-        <motion.p className="text-4xl font-medium"
+        <motion.p
+          className="text-4xl font-medium"
           variants={variants}
           initial="hidden"
           animate="visible"
-          transition={{ delay: 1 }}> Hola! Soy Ruben </motion.p>
+          transition={{ delay: 1 }}
+        >
+          {" "}
+          Hola! Soy Ruben{" "}
+        </motion.p>
         <div>
-          <motion.p className="text-5xl font-black text-neutral-300"
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          transition={{ delay: 1.2 }}>
+          <motion.p
+            className="text-5xl font-black text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.2 }}
+          >
             Building
           </motion.p>
           <div>
@@ -68,11 +86,13 @@ export const HeroText = () => {
               className="font-bold text-white text-7xl"
             />
           </div>
-          <motion.p className="text-axl font-black text-neutral300"
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          transition={{ delay: 1.4 }}>
+          <motion.p
+            className="text-axl font-black text-neutral300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.4 }}
+          >
             Web Applications
           </motion.p>
         </div>
